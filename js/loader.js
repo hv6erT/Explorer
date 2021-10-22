aeb4860268a79cdc03a8c99e5073ae86df9354b9
hv6erT <92681087+hv6erT@users.noreply.github.com>
2021-10-22 20:21:32 +0200

    Start page creation

--- js/loader.js
+++ js/loader.js
@@ -1,17 +1,21 @@
 let GameMap
-const load = async ()=>{
+const loadGame = async ()=>{
   	const gameWrapperNode = document.getElementById("game-wrapper")
   	const startWrapperNode = document.getElementById("start-wrapper")
 
   	startWrapperNode.style.display="none"
   	gameWrapperNode.style.display="block"
+
+  	startWrapperNode.addEventListener("click", startGame)
   
   	GameMap = await import("./modules/game-map.js")
+}
 
+window.addEventListener("DOMContentLoaded", loadGame)
+
+const startGame = async ()=>{
   	gameWrapperNode.style.display="none"
   	startWrapperNode.style.display="block"
 }
-
-window.addEventListener("DOMContentLoaded", load)
 
 
