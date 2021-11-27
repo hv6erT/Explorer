1596b8e6f71ca6056d13727a6695ca2bb44a9745
hv6erT <92681087+hv6erT@users.noreply.github.com>
2021-11-27 17:26:42 +0100

    Repair walking into ground bug v6

--- engine/vector.ts
+++ engine/vector.ts
@@ -24,21 +24,24 @@ export default class Vector {
           	
     	const playerPxPosition: {x: number; y: number} = this.player.getPosition()
 
+    	const playerWidth = parseFloat(getComputedStyle(this.player.getDom()).width)
+    	const playerHeight = parseFloat(getComputedStyle(this.player.getDom()).height)
+
     	const mapBlockSize = map.getBlockSize()
 
     	const self = this
 		const gravity = function(){
-          	let blockAtBottom = map.getBlockAtPxPosition(playerPxPosition.x, playerPxPosition.y+mapBlockSize+1)
+          	let blockAtBottom = map.getBlockAtPxPosition(playerPxPosition.x, playerPxPosition.y+(playerHeight/2)+(mapBlockSize/2)+1)
           
           	while(blockAtBottom!=null && blockAtBottom.type === "penetrable"){
               	playerPxPosition.y++
               	self.player.getDom().style.top = `${playerPxPosition.y}px`
-              	blockAtBottom = map.getBlockAtPxPosition(playerPxPosition.x, playerPxPosition.y+mapBlockSize+1)
+              	blockAtBottom = map.getBlockAtPxPosition(playerPxPosition.x, playerPxPosition.y+(playerHeight/2)+(mapBlockSize/2)+1)
             }
         }
     
 		const goForward = function () {
-          	const nextBlock = map.getBlockAtPxPosition(playerPxPosition.x+mapBlockSize+1, playerPxPosition.y)
+          	const nextBlock = map.getBlockAtPxPosition(playerPxPosition.x+(playerWidth/2)+(mapBlockSize/2)+1, playerPxPosition.y)
 
           	if(nextBlock.type === "penetrable"){
               	playerPxPosition.x++
@@ -47,7 +50,7 @@ export default class Vector {
 		}
 
     	const goBackward = function () {
-    	  	const previousBlock = map.getBlockAtPxPosition(playerPxPosition.x-mapBlockSize-1, playerPxPosition.y)
+    	  	const previousBlock = map.getBlockAtPxPosition(playerPxPosition.x-(playerWidth/2)-(mapBlockSize/2)-1, playerPxPosition.y)
 
           	if(previousBlock.type === "penetrable"){
               	playerPxPosition.x--
